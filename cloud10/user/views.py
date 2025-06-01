e4ec0459b1b6044cd32929b0d8e70bfa2548439e
an <dmlcks0209@naver.com>
2025-06-02 00:09:32 +0900

    feat: 회원가입 기능 추가

--- cloud10/user/views.py
+++ cloud10/user/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, SignupSerializer
 
 
 # 유저 정보 조회
@@ -10,10 +10,23 @@ class UserInfoView(APIView):
     def get(self, request):
         try:
             users = User.objects.all()
-            serializer = UserSerializer(users, many=True)
-            return Response({'users': serializer.data})
+            user_data = [{
+                'id': user.id,
+                'username': user.username,
+                'created_at': user.created_at
+            } for user in users]
+            return Response(user_data, status=status.HTTP_200_OK)
         except Exception as e:
-            return Response(
-                {'error': str(e)},
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR
-            )
+            return Response({
+                'message': str(e)
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class SignupView(APIView):
+    def post(self, request):
+        serializer = SignupSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'message': '회원가입이 완료되었습니다.'
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
