5277d7c66f9bf6997111c9140bcfe3e745224526
an <dmlcks0209@naver.com>
2025-06-02 15:17:54 +0900

    refactor: counsel_id 변수 이름 수정

--- cloud10/chat/views.py
+++ cloud10/chat/views.py
@@ -7,5 +7,5 @@ class RandomHashView(APIView):
         """c_로 시작하는 랜덤 해시값 생성"""
         random_hash = f"c_{uuid.uuid4().hex[:16]}"
         return Response({
-            'hash': random_hash
+            'counsel_id': random_hash
         })
